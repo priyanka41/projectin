be04e295e7def181d744601a6cf7e8513b0a1d38
puzarana <magarpuza06@gmail.com>
2021-08-20 22:13:09 +0545

    header,footer

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="topnav">
+  <input type="text" placeholder="What you wants?">
+</div>
+
+<div class="text-center">
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime dolore quod .</p>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome'); // welcome.blade.php
+    return view('home'); // welcome.blade.php
 });
 
 Route::view('/result', 'search'); //1. path, 2. search.blade.php
